b2bb641bb53edd6c5a51580529f459acf7fbff36
Dmitri Smirnov <dmitrism@microsoft.com>
2024-11-01 16:52:40 -0700

    Capacity Aware Parititioning begins Implement GetSizeFromTensorTypeProo Wire in accounting Make CUDA EP resource aware and account on assignment Fix missing accountant for Ort format Remove redundant functions Remove unnecessary interface Fix DML issue, minor fixes Fix alert DEMO changes

--- onnxruntime/core/providers/nnapi/nnapi_builtin/nnapi_execution_provider.cc
+++ onnxruntime/core/providers/nnapi/nnapi_builtin/nnapi_execution_provider.cc
@@ -80,8 +80,8 @@ NnapiExecutionProvider::~NnapiExecutionProvider() {}
 
 std::vector<std::unique_ptr<ComputeCapability>>
 NnapiExecutionProvider::GetCapability(const onnxruntime::GraphViewer& graph_viewer,
-                                      const IKernelLookup& /*kernel_lookup*/) const {
-  const auto& logger = *GetLogger();
+                                      const IKernelLookup& /*kernel_lookup*/,
+                                      IResourceAccountant* /* resource_accountant */) const {
   std::vector<std::unique_ptr<ComputeCapability>> result;
 
   // TODO: Task 812756: NNAPI EP, add support for subgraph (If and Loop operators)
