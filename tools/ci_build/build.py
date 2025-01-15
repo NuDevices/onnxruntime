1c58fee656aed5086ee381dbe9f5c7ea699f43c6
Karim Vadsariya <kvadsariya@microsoft.com>
2025-01-15 15:23:20 -0800

    Fix Pipeline issues with QNN

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -1314,6 +1314,7 @@ def generate_build_tree(
             cmake_args += ["-DCMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM=" + args.xcode_code_signing_team_id]
 
     if args.use_qnn:
+
         if args.qnn_home is None or os.path.exists(args.qnn_home) is False:
             raise BuildError("qnn_home=" + qnn_home + " not valid." + " qnn_home paths must be specified and valid.")
         cmake_args += ["-Donnxruntime_USE_QNN=ON"]
@@ -2713,8 +2714,9 @@ def main():
     armnn_home = args.armnn_home
     armnn_libs = args.armnn_libs
 
-    qnn_home = args.qnn_home
     qnn_home = ""
+    if args.use_qnn:
+        qnn_home = args.qnn_home
 
     # if using tensorrt, setup tensorrt paths
     tensorrt_home = ""
